3406e0743d757b86fee2d1336ec5d3d001e350bf
Edina Szabó <szabo.edina.1229@gmail.com>
2020-01-15 17:19:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fragaria/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.fragaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bttnNovenyNevelo, bttnShop, bttGaleria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        bttnNovenyNevelo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,NovenyNeveloActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bttGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GaleriaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bttnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ShopActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    public void init(){
+        bttnNovenyNevelo = findViewById(R.id.bttnNovenyNevelo);
+        bttGaleria = findViewById(R.id.bttnGaleria);
+        bttnShop = findViewById(R.id.bttnShop);
+    }
+}
---
+++ app/src/main/java/com/example/fragaria/NovenyNeveloActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovenyNeveloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noveny_nevelo);
+    }
+}
