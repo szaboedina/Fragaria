df39461e500566e75c098a7a29775dcf2a31eb37
Edina Szabó <szabo.edina.1229@gmail.com>
2020-01-16 17:11:33 +0100

    table layout

--- app/src/main/java/com/example/fragaria/NovenyValasztoActivity.java
+++ app/src/main/java/com/example/fragaria/NovenyValasztoActivity.java
@@ -2,13 +2,30 @@ package com.example.fragaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NovenyValasztoActivity extends AppCompatActivity {
+    private Button bttnVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noveny_valaszto);
+        init();
+        bttnVissza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NovenyValasztoActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    public void init(){
+        bttnVissza = findViewById(R.id.bttnVissza);
+
     }
 }
